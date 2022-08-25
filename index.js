337f214c2051636b531b5f5ff72f9d0ec176cddf
Noahkimtai <noahkimtai24@gmail.com>
2022-08-25 16:26:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('The DOM content has already loaded')
+    let p = document.querySelector('#text')
+    p.textContent='This is really cool!'
+})// Ensures the the javascript only loads after the content has loaded
+// You can not manipulate what has not been loaded to the DOM
+// it does not wait for imgs and stylesheet to load
